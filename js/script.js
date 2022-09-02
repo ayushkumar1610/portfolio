00adc18efdadd087a0c61ca25e1cfcca2727439f
Ayush <ayushkumar1610@outlook.com>
2022-09-02 19:49:25 +0530

    rotate text added

--- js/script.js
+++ js/script.js
@@ -52,3 +52,5 @@ function myFunction(x) {
 var x = window.matchMedia("(max-width: 700px)")
 myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes
+
+
